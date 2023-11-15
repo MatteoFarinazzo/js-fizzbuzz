2d6c6b90e35b33d1512b63562e7ac65d8e7470cd
MatteoFarinazzo <matteofary@gmail.com>
2023-11-15 15:38:57 +0100

    Finished +(shown in html)

--- js/main.js
+++ js/main.js
@@ -5,22 +5,29 @@ for (let i = 1; i<101; i++){
     
     //individuare numeri multipli sia di 3 che di 5
     if ((i%3==0) && (i%5==0)){
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
+        document.getElementById("numeri").innerHTML += " " + "&nbsp" + "FizzBuzz";
     }
 
     //individure numeri multipli di 5 con il mudulo
     else if(i%5==0){
-        console.log("Buzz")
+        console.log("Buzz");
+        document.getElementById("numeri").innerHTML += " " + "&nbsp" + "Buzz";
     }
 
     // individuare numeri multipli di 3
     else if (i%3==0){
-        console.log("fizz")
+        console.log("Fizz");
+        document.getElementById("numeri").innerHTML += " " + "&nbsp" + "Fizz";
     }
-    else console.log(i)
+
+    else console.log(i); document.getElementById("numeri").innerHTML += " " + "&nbsp" + i;
 
 
-}
+    }
+
+
+
 
 
 
