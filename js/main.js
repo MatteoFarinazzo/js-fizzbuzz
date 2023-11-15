a54437d96e07af1c8ad08ec9471be001717ff710
MatteoFarinazzo <matteofary@gmail.com>
2023-11-15 13:54:39 +0100

    started

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// ciclo che crea i numeri da 1 a 100
+
+for (let i = 1; i<101; i++){
+
+    console.log(i);
+    // individuare numeri multipli di 3
+
+    if (i%3){
+        console.log("fizz")
+    }
+
+
+
+}
+
+
+
+//individure numeri multipli di 5 con il mudulo
+
+//individuare numeri multipli sia di 3 che di 5
+
